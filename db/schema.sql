350549e3bae001d95555551a5d76cf790b2924e8
LunaRossie <>
2022-07-10 12:25:01 -0400

    add to files

--- db/schema.sql
+++ db/schema.sql
@@ -2,9 +2,16 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 USE employee_db; 
 
-CREATE TABLE department (
+CREATE TABLE employee (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER, 
+    INDEX role_ind (role_id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    manager_id INTEGER,
+    INDEX manager_ind (manager_id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
 
 CREATE TABLE role (
@@ -16,14 +23,7 @@ CREATE TABLE role (
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
-CREATE TABLE employee (
+CREATE TABLE department (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER, 
-    INDEX role_ind (role_id),
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
-    manager_id INTEGER,
-    INDEX manager_ind (manager_id),
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    name VARCHAR(30) NOT NULL
 );
---
+++ server.js
@@ -0,0 +1,23 @@
+// import mysql2
+const mysql = require('mysql2')
+// import inquirer 
+const inquirer = require('inquirer'); 
+// import console.table
+const cTable = require('console.table'); 
+
+require('dotenv').config()
+
+// connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'rootroot',
+  database: 'employee_db'
+});
+
+connection.connect(err => {
+  if (err) throw err;
+  console.log('connected as id' + connection.threadId);
+  afterConnection();
+});
+
